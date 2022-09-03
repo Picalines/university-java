3ced04b81a2c8913f80468707fbd383aee9d978c
Picalines <picalines0@gmail.com>
2022-09-03 13:44:51 +0300

    practice 3 (WIP)

---
+++ src/main/java/practice3/Rectangle.java
@@ -0,0 +1,55 @@
+package practice3;
+
+public class Rectangle extends Shape {
+    protected double width;
+    protected double length;
+
+    public Rectangle(double width, double length, String color, boolean filled) {
+        super(color, filled);
+        setWidth(width);
+        setLength(length);
+    }
+
+    public Rectangle(double width, double length) {
+        super();
+        setWidth(width);
+        setLength(length);
+    }
+
+    public Rectangle() {
+        super();
+        setWidth(1);
+        setLength(1);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public double getArea() {
+        return width * length;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return (width + length) * 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle(width: " + width + ", length: " + length + ", color: " + color + ", filled: " + filled + ")";
+    }
+}
---
+++ src/main/java/practice3/Square.java
@@ -0,0 +1,39 @@
+package practice3;
+
+public class Square extends Rectangle {
+    public Square() {
+        super(1, 1);
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(double side, String color, boolean filled) {
+        super(side, side, color, filled);
+    }
+
+    public double getSide() {
+        return width;
+    }
+
+    public void setSide(double side) {
+        super.setWidth(side);
+        super.setLength(side);
+    }
+
+    @Override
+    public void setWidth(double width) {
+        setSide(width);
+    }
+
+    @Override
+    public void setLength(double length) {
+        setSide(length);
+    }
+
+    @Override
+    public String toString() {
+        return "Square(side: " + getSide() + ", color: " + color + ", filled: " + filled + ")";
+    }
+}
