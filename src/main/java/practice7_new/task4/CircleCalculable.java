cab95d7404c08d70d86f44173b09581d9329bbce
Picalines <picalines0@gmail.com>
2022-12-15 19:39:19 +0300

    practice7_new fix

--- src/main/java/practice7_new/task4/CircleCalculable.java
+++ src/main/java/practice7_new/task4/CircleCalculable.java
@@ -3,12 +3,28 @@ package practice7_new.task4;
 public class CircleCalculable implements MathCalculable {
     @Override
     public Complex pow(Complex complex, int base) {
-        throw new RuntimeException("why???");
+        return switch (base) {
+            case 1 -> complex;
+            case 0 -> new Complex(0, 0);
+            default -> {
+                var sq = pow(complex, base / 2);
+                yield base % 2 == 0
+                    ? multiply(sq, sq)
+                    : multiply(complex, multiply(sq, sq));
+            }
+        };
+    }
+
+    private Complex multiply(Complex a, Complex b) {
+        return new Complex(
+            (a.real() * b.real()) - (a.imaginary() * b.imaginary()),
+            (a.imaginary() * b.real()) + (a.real() * b.imaginary())
+        );
     }
 
     @Override
     public double abs(Complex complex) {
-        throw new RuntimeException("why???");
+        return Math.sqrt(complex.real() * complex.real() + complex.imaginary() * complex.imaginary());
     }
 
     public double length(double r) {
@@ -17,6 +33,10 @@ public class CircleCalculable implements MathCalculable {
 
     public static void main(String[] args) {
         var cc = new CircleCalculable();
-        System.out.println(cc.length(5));
+        System.out.println("Circle length = " + cc.length(5) + ", r = 5");
+        var c1 = new Complex(1, 2);
+        System.out.println("z = " + c1);
+        System.out.println("|z| = " + cc.abs(c1));
+        System.out.println("z^2 = " + cc.pow(c1, 2));
     }
 }
