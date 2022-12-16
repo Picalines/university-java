7fb1777c37ac53cced78530ad5bbdd92a10ac632
Picalines <picalines0@gmail.com>
2022-12-16 08:58:47 +0300

    practice14 output fixes

--- src/main/java/practice14/exercise5/Main.java
+++ src/main/java/practice14/exercise5/Main.java
@@ -11,7 +11,6 @@ public class Main {
         var startDate = LocalDate.parse("1990-01-01");
         var endDate = LocalDate.parse("9999-12-31");
 
-        System.out.println("building regex...");
         regexBuilder.append('^');
 
         for (var date = startDate; date.isBefore(endDate); date = date.plusDays(1))
@@ -28,7 +27,6 @@ public class Main {
 
         regexBuilder.append("(31/12/9999)$");
 
-        System.out.println("compiling...");
         var pattern = Pattern.compile(regexBuilder.toString());
 
         var scanner = new Scanner(System.in);
